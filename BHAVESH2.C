eedf75b13de5562da2922dfa744ad29ff5013e0c
vidhi parikh <vidhi.parikh2001@gmail.com>
2024-04-11 15:56:07 +0530

    c project

---
+++ BHAVESH2.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a = 12;
+int b = 6;
+
+
+clrscr();
+
+printf("%d\n",a+b);
+printf("%d\n",a-b);
+printf("%d\n",a*b);
+printf("%d\n",a/b);
+printf("%d\n",a%b);
+
+getch();
+}
